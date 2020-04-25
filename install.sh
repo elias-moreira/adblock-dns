64ce4baef0bba65a0ac1fd7f1f92c241a1a14510
Pedro Henrique Vilas Boas <40247368+phvilasboas@users.noreply.github.com>
2020-04-25 18:56:00 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ apt install bind9 dnsutils -y
 mkdir /var/cache/bind/rpz/
 ln -s /var/cache/bind/rpz/ /etc/bind/
 cp -r bind/* /etc/bind
+chmod +x /etc/bind/script-rpz/gera.sh
 /etc/bind/script-rpz/gera.sh
 echo "FIM"
